88d5e0b7000be3c2a9ffde527a03620bb14118e2
Anna <Anna@ANNA-PC>
2017-05-31 22:42:27 +0300

    биндинг3

--- WpfApplication1/WpfApplication1/MainWindowViewModel.cs
+++ WpfApplication1/WpfApplication1/MainWindowViewModel.cs
@@ -4,25 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.ComponentModel;
 
 
 namespace WpfApplication1
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel:INotifyPropertyChanged
     {
-        public string avtor1 { get; set; }
-        public string avtor2 { get; set; }
+        public string avtor
+        {
+            get { return _author; }
+            set
+            {
+                _author = value;
+                Notify(nameof(avtor));
+            }
+        }
+       
         public string novavtor { get; set; }
         public string namebook { get; set; }
         public string nameizdatel { get; set; }
-        public string nameyear { get; set; }
-        public int tiraj { get; set; }
+       
+        public int year
+        {
+            get { return _year; }
+            set
+            {
+                if (value <= DateTime.Now.Year)
+                {
+                    _year = value;
+                    Notify(nameof(year));
+                }
+            }
+        }
+        
+        public int tiraj
+        {
+            get { return _tiraj; }
+            set
+            {
+                if (value > 0)
+                {
+                    _tiraj = value;
+                    Notify(nameof(tiraj));
+                }
+                else
+                {
+                    
+                }
+            }
+        }
         public string isbn { get; set; }
         public ICommand save { get; set; }
         public ICommand otmena { get; set; }
 
+        private int _tiraj = 1;
+        private int _year = DateTime.Now.Year;
+        private string _author;
 
 
-
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Notify(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
     }
+
 }
